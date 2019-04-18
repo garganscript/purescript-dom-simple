8360df46109caa78a3cd053d733c8938506b8813
James Laver <james.laver@gmail.com>
2019-04-18 09:23:46 +0200

    Export DOM.Simple.Types from DOM.Simple

--- src/DOM/Simple.purs
+++ src/DOM/Simple.purs
@@ -1,10 +1,12 @@
 module DOM.Simple
-  ( module DOM.Simple.Element
+  ( module DOM.Simple.Types
+  , module DOM.Simple.Element
   , module DOM.Simple.Document
   , module DOM.Simple.Node
   , module DOM.Simple.Window )
   where
 
+import DOM.Simple.Types
 import DOM.Simple.Element
 import DOM.Simple.Document
   ( createElement, createElement', createTextNode, document, global )
