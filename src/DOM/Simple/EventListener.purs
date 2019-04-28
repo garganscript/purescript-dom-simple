6587bf2762a7cd3d518e0c2b32988725881177e6
James Laver <james.laver@gmail.com>
2019-04-28 13:39:19 +0200

    Add D.S.EventListener.{add,remove}EventListener

--- src/DOM/Simple/EventListener.purs
+++ src/DOM/Simple/EventListener.purs
@@ -1,13 +1,14 @@
 module DOM.Simple.EventListener
-  ( class EventListener
-  , dispatchEvent -- , addEventListener, removeEventListener
+  ( class EventListener, Callback
+  , dispatchEvent, addEventListener, removeEventListener
   ) where
 
-import Prelude ( Unit, ($) )
+import Prelude ( Unit, ($), (<<<), pure, unit )
 import DOM.Simple.Types ( Element )
+import DOM.Simple.Event ( class IsEvent )
 import Effect ( Effect )
-import Effect.Uncurried ( EffectFn2, runEffectFn2 )
-import FFI.Simple ( (...) )
+import Effect.Uncurried ( EffectFn1, mkEffectFn1 )
+import FFI.Simple ( (...), delay, args2 )
 
 class EventListener e
 
@@ -16,5 +17,30 @@ instance eventListenerElement :: EventListener Element
 dispatchEvent :: forall l e. EventListener l => l -> e -> Effect Unit
 dispatchEvent l e = l ... "dispatchEvent" $ [e]
 
--- addEventListener
--- removeEventListener
+newtype Callback a = Callback (EffectFn1 a Unit)
+
+callback :: forall a. (a -> Effect Unit) -> Callback a
+callback = Callback <<< mkEffectFn1
+
+-- | A little unsafe
+addEventListener
+  :: forall listener event
+  .  EventListener listener
+  => IsEvent event
+  => listener
+  -> String
+  -> Callback event
+  -> Effect Unit
+addEventListener obj name callback =
+  delay unit $ \_ -> pure $ obj ... "addEventListener" $ args2 name callback
+
+removeEventListener
+  :: forall listener event
+  .  EventListener listener
+  => IsEvent event
+  => listener
+  -> String
+  -> Callback event
+  -> Effect Unit
+removeEventListener obj name callback =
+  delay unit $ \_ -> pure $ obj ... "removeEventListener" $ args2 name callback
