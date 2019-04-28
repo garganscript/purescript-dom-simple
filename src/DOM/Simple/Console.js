987fe4161a1e6c8d85fb8bad62452e2e02b943db
James Laver <james.laver@gmail.com>
2019-04-28 13:36:44 +0200

    Move D.S.Console to ffi-simple

--- src/DOM/Simple/Console.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-exports._log = console.log;
-exports._log2 = console.log;
-exports._log3 = console.log;
-exports._log4 = console.log;
-exports._log5 = console.log;
-exports._log6 = console.log;
-exports._log7 = console.log;
-exports._log8 = console.log;
--- src/DOM/Simple/Console.purs
+++ src/DOM/Simple/Console.purs
@@ -1,35 +1,34 @@
 module DOM.Simple.Console where
 
-import Data.Unit ( Unit )
+import Prelude ( Unit, unit, ($), pure )
 import Effect ( Effect )
 import Effect.Uncurried
   ( EffectFn1, runEffectFn1, EffectFn2, runEffectFn2
   , EffectFn3, runEffectFn3, EffectFn4, runEffectFn4
   , EffectFn5, runEffectFn5, EffectFn6, runEffectFn6
   , EffectFn7, runEffectFn7, EffectFn8, runEffectFn8 )
+import FFI.Simple
+ ( (...), delay, unsafeGlobal
+ , args2, args3, args4, args5, args6, args7, args8 )
+
+foreign import data Console :: Type
+
+console :: Console
+console = unsafeGlobal "console"
 
 log :: forall a. a -> Effect Unit
-log = runEffectFn1 _log
+log a = delay unit $ \_ -> pure $ console ... "log" $ [a]
 log2 :: forall a b. a -> b -> Effect Unit
-log2 = runEffectFn2 _log2
+log2 a b = delay unit $ \_ -> pure $ console ... "log" $ args2 a b
 log3 :: forall a b c. a -> b -> c -> Effect Unit
-log3 = runEffectFn3 _log3
+log3 a b c = delay unit $ \_ -> pure $ console ... "log" $ args3 a b c
 log4 :: forall a b c d. a -> b -> c -> d -> Effect Unit
-log4 = runEffectFn4 _log4
+log4 a b c d = delay unit $ \_ -> pure $ console ... "log" $ args4 a b c d
 log5 :: forall a b c d e. a -> b -> c -> d -> e -> Effect Unit
-log5 = runEffectFn5 _log5
+log5 a b c d e = delay unit $ \_ -> pure $ console ... "log" $ args5 a b c d e
 log6 :: forall a b c d e f. a -> b -> c -> d -> e -> f -> Effect Unit
-log6 = runEffectFn6 _log6
+log6 a b c d e f = delay unit $ \_ -> pure $ console ... "log" $ args6 a b c d e f
 log7 :: forall a b c d e f g. a -> b -> c -> d -> e -> f -> g -> Effect Unit
-log7 = runEffectFn7 _log7
+log7 a b c d e f g = delay unit $ \_ -> pure $ console ... "log" $ args7 a b c d e f g
 log8 :: forall a b c d e f g h. a -> b -> c -> d -> e -> f -> g -> h -> Effect Unit
-log8 = runEffectFn8 _log8
-
-foreign import _log :: forall a. EffectFn1 a Unit
-foreign import _log2 :: forall a b. EffectFn2 a b Unit
-foreign import _log3 :: forall a b c. EffectFn3 a b c Unit
-foreign import _log4 :: forall a b c d. EffectFn4 a b c d Unit
-foreign import _log5 :: forall a b c d e. EffectFn5 a b c d e Unit
-foreign import _log6 :: forall a b c d e f. EffectFn6 a b c d e f Unit
-foreign import _log7 :: forall a b c d e f g. EffectFn7 a b c d e f g Unit
-foreign import _log8 :: forall a b c d e f g h. EffectFn8 a b c d e f g h Unit
+log8 a b c d e f g h = delay unit $ \_ -> pure $ console ... "log" $ args8 a b c d e f g h
