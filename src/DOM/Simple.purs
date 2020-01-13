c70d2745c1f92239d00e5c7f852fd716e4e00011
justinwoo <moomoowoo@gmail.com>
2020-01-13 19:29:05 +0200

    updates

--- src/DOM/Simple.purs
+++ src/DOM/Simple.purs
@@ -1,15 +1,11 @@
 module DOM.Simple
-  ( module DOM.Simple.Types
-  , module DOM.Simple.Element
-  , module DOM.Simple.Document
-  , module DOM.Simple.Node
-  , module DOM.Simple.Window )
+  ( module Exports
+  )
   where
 
-import DOM.Simple.Types
-import DOM.Simple.Element
-import DOM.Simple.Document
-  ( createElement, createElement', createTextNode, document )
-import DOM.Simple.Node
-import DOM.Simple.Window ( Window, window, global )
+import DOM.Simple.Types (class IsChild, class IsElement, class IsEventListener, class IsNode, class IsParent, class IsSibling, DOMRect, Document, Element, Fragment, Text) as Exports
+import DOM.Simple.Element (Callback, addEventListener, after, append, attr, attrNames, before, boundingRect, callback, childCount, children, dispatchEvent, firstChildElement, hasAttr, innerHTML, lastChildElement, nextElement, prepend, prevElement, querySelector, querySelectorAll, remove, removeEventListener, replaceWith, setAttr) as Exports
+import DOM.Simple.Document ( createElement, createElement', createTextNode, document ) as Exports
+import DOM.Simple.Node (contains, name, nextSibling, ownerDocument, parentElement, parentNode, prevSibling, textContent) as Exports
+import DOM.Simple.Window ( Window, window, global ) as Exports
 
